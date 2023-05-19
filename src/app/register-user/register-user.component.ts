91366856909f5732e1af5263e0a22095b8cf372e
HarshRajbhar <hkrajbhar2002@gmail.com>
2023-05-19 17:35:12 +0530

    register user design completed

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+interface table {
+  name: string;
+  city: string;
+  email: string;
+  phone: string;
+}
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule],
   templateUrl: './register-user.component.html',
-  styleUrls: ['./register-user.component.scss']
+  styleUrls: ['./register-user.component.scss'],
 })
 export class RegisterUserComponent {
-
+  reg_User: table = {
+    name: '',
+    email: '',
+    phone: '',
+    city: '',
+  };
+  submit() {
+    if (
+      this.reg_User.name.trim() !== '' &&
+      this.reg_User.email.trim() !== '' &&
+      this.reg_User.phone.trim() !== '' &&
+      this.reg_User.city.trim() !== ''
+    ) {
+      console.log(this.reg_User);
+    }
+  }
 }
