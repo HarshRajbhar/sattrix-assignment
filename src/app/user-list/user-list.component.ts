b90f7edec460b5057cf59565de54e124c22e7d31
HarshRajbhar <hkrajbhar2002@gmail.com>
2023-05-19 17:29:24 +0530

    user list design completed

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,13 +1,35 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+interface table {
+  name: string;
+  city: string;
+  email: string;
+  phone: string;
+}
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgFor],
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent {
+  user_list: table[] = [
+    {
+      name: 'Harsh Kumar Rajbhar',
+      email: 'hkrajbhar2002@gmail.com',
+      phone: '8866128435',
+      city: 'Ahmedabad',
+    },
+  ];
 
+  edit(id: number) {
+    console.log('Edit' + ' ' + id);
+  }
+
+  dlt(id: number) {
+    this.user_list.splice(id, 1);
+    console.log(id);
+  }
 }
