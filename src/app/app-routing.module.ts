a030962deff523ea7e5bc667c3b6cc8b65f45ecb
HarshRajbhar <hkrajbhar2002@gmail.com>
2023-05-19 16:26:40 +0530

    landing page design completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./login/login.component').then((a) => a.LoginComponent),
+    component: LandingPageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./login/login.component').then((a) => a.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./register/register.component').then(
+            (a) => a.RegisterComponent
+          ),
+      },
+    ],
   },
 ];
 
